df956ff934251aff8189a9b24ff4fb5fd8bb9e7a
Alessandro L. Santos <aleluizsantos@gmail.com>
2021-11-01 15:54:34 -0300

    ajusted #002 - router Top10

--- src/database/seeds/09_create_Opening_Hours.js
+++ src/database/seeds/09_create_Opening_Hours.js
@@ -1,11 +1,11 @@
 exports.seed = async function (knex) {
   await knex("openingHours").insert([
-    { week: "Domingo", week_id: "0", start: "08:00", end: "13:00" },
-    { week: "Segunda-feira", week_id: "1", start: "08:00", end: "19:00" },
-    { week: "Terça-feira", week_id: "2", start: "08:00", end: "19:00" },
-    { week: "Quarta-feira", week_id: "3", start: "08:00", end: "19:00" },
-    { week: "Quinta-feira", week_id: "4", start: "08:00", end: "19:00" },
-    { week: "Sexta-feira", week_id: "5", start: "08:00", end: "19:00" },
-    { week: "Sábado", week_id: "6", start: "08:00", end: "18:00" },
+    { week: "Domingo", week_id: "0", start: "19:00", end: "23:00" },
+    { week: "Segunda-feira", week_id: "1", start: "19:00", end: "23:00" },
+    { week: "Terça-feira", week_id: "2", start: "", end: "" },
+    { week: "Quarta-feira", week_id: "3", start: "", end: "" },
+    { week: "Quinta-feira", week_id: "4", start: "19:00", end: "23:00" },
+    { week: "Sexta-feira", week_id: "5", start: "19:00", end: "00:00" },
+    { week: "Sábado", week_id: "6", start: "19:00", end: "00:00" },
   ]);
 };
